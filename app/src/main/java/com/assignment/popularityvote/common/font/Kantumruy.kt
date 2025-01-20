654f4c23b810b84e63aefa26bc95f1a8bf799157
ImGaram <gim78014@gmail.com>
2025-01-20 17:50:02 +0900

    :lipstick: :: Kantumruy font 추가 및 font family 생성

---
+++ app/src/main/java/com/assignment/popularityvote/common/font/Kantumruy.kt
@@ -0,0 +1,24 @@
+package com.assignment.popularityvote.common.font
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.assignment.popularityvote.R
+
+val Kantumruy = FontFamily(
+    Font(R.font.kantumruy_pro_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.kantumruy_pro_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.kantumruy_pro_extra_light, FontWeight.ExtraLight, FontStyle.Normal),
+    Font(R.font.kantumruy_pro_extra_light_italic, FontWeight.ExtraLight, FontStyle.Italic),
+    Font(R.font.kantumruy_pro_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.kantumruy_pro_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.kantumruy_pro_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.kantumruy_pro_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.kantumruy_pro_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.kantumruy_pro_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.kantumruy_pro_semibold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.kantumruy_pro_semibold_italic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.kantumruy_pro_thin, FontWeight.Thin, FontStyle.Normal),
+    Font(R.font.kantumruy_pro_thin_italic, FontWeight.Thin, FontStyle.Italic),
+)
