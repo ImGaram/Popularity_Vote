8774c5c7ae6ebed7c05ab364668d868dc2222b1b
ImGaram <gim78014@gmail.com>
2025-01-20 17:50:15 +0900

    :lipstick: :: color 추가

--- app/src/main/java/com/assignment/popularityvote/ui/theme/Color.kt
+++ app/src/main/java/com/assignment/popularityvote/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Primary = Color(0xFF4232D5)
+val Background = Color(0xFF060203)
+val BackgroundLight = Color(0xFF1E1E1E)
+val White = Color(0xFFFFFFFF)
