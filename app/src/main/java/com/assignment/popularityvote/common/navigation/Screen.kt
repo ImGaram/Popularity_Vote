e86ec2e44b612b9825f4f7b19037b46bec66f004
ImGaram <gim78014@gmail.com>
2025-01-21 17:03:09 +0900

    :sparkles: :: navigate을 위한 kotlinx serialization 적용

--- app/src/main/java/com/assignment/popularityvote/common/navigation/Screen.kt
+++ app/src/main/java/com/assignment/popularityvote/common/navigation/Screen.kt
@@ -1,7 +1,14 @@
 package com.assignment.popularityvote.common.navigation
 
+import kotlinx.serialization.Serializable
+
 sealed class Screen {
+    @Serializable
     data object LoginScreen: Screen()
+
+    @Serializable
     data object MainScreen: Screen()
+
+    @Serializable
     data object ProfileScreen: Screen()
 }
