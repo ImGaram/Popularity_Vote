934b498cb3ece3e0b94f836d9fe57e093ea02a02
ImGaram <gim78014@gmail.com>
2025-01-21 17:04:55 +0900

    :sparkles: :: log in 기능 구현

--- app/src/main/java/com/assignment/popularityvote/feature/login/LoginScreen.kt
+++ app/src/main/java/com/assignment/popularityvote/feature/login/LoginScreen.kt
@@ -1,15 +1,89 @@
 package com.assignment.popularityvote.feature.login
 
+import androidx.activity.compose.LocalActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.assignment.popularityvote.R
+import com.assignment.popularityvote.common.compose.PopularityVoteButton
+import com.assignment.popularityvote.common.compose.TopBar
+import com.assignment.popularityvote.common.font.Kantumruy
+import com.assignment.popularityvote.feature.util.UserId
+import com.assignment.popularityvote.ui.theme.Background
+import com.assignment.popularityvote.ui.theme.Primary
+import com.assignment.popularityvote.ui.theme.White
 
 @Composable
-fun LoginScreen() {
+fun LoginScreen(
+    navigateToMain: () -> Unit,
+) {
+    val idState = remember { mutableStateOf("") }
+    val activity = LocalActivity.current
 
-}
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Background),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TopBar(
+            modifier = Modifier.fillMaxWidth(),
+            onExitClick = {
+                activity?.finish()
+            }
+        )
 
-@Preview
-@Composable
-private fun LoginScreenPreview() {
-    LoginScreen()
+        Image(
+            painter = painterResource(R.drawable.img_promote_post),
+            contentDescription = "promote post image",
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
+
+        LoginTextField(
+            value = idState.value,
+            onValueChange = {
+                idState.value = it
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, top = 32.dp, end = 16.dp)
+        )
+
+        PopularityVoteButton(
+            onClick = {
+                UserId.setUserId(idState.value)
+
+                if (UserId.getInstance() != null) navigateToMain()
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, top = 24.dp, end = 16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Primary
+            ),
+            text = "Log in",
+            textStyle = TextStyle(
+                color = White,
+                fontFamily = Kantumruy,
+                fontWeight = FontWeight.W700,
+                fontSize = 16.sp
+            )
+        )
+    }
 }
