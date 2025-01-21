f40c8ea61219228c79cb801ccb1599f93b987110
ImGaram <gim78014@gmail.com>
2025-01-22 01:23:20 +0900

    :sparkles: :: Main에서 전달받은 인자 확인 테스트

--- app/src/main/java/com/assignment/popularityvote/feature/profile/ProfileScreen.kt
+++ app/src/main/java/com/assignment/popularityvote/feature/profile/ProfileScreen.kt
@@ -1,10 +1,15 @@
 package com.assignment.popularityvote.feature.profile
 
 import androidx.compose.runtime.Composable
+import com.assignment.popularityvote.common.compose.PopularityVoteText
+import com.assignment.popularityvote.common.navigation.Screen
 
 @Composable
 fun ProfileScreen(
-    navigateToMain: () -> Unit
+    navigateToMain: () -> Unit,
+    args: Screen.ProfileScreen
 ) {
-
+    PopularityVoteText(
+        text = args.id.toString()
+    )
 }
